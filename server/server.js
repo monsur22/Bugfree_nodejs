d4ceaf326731cb608f94f513235dfee4ec76ac87
monsur22 <monsurahmedshafiq@gmail.com>
2021-11-16 23:21:02 +0600

    setup server.js

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyparser from 'body-parser'
+// import Routes from './routes/Routes.js'
+
+
+const port = 3000;
+const app = express();
+
+app.listen(port, function() {
+	console.log("Server is listening at port:" + port);
+});
+
+// Parses the text as url encoded data
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// Parses the text as json
+// app.use(bodyParser.json());
+
+// app.get('/',(req,res) => {
+//     res.send('Server Is Running .......3000 Port')
+//     })
+// app.use('/', Routes);
